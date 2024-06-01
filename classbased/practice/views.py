324357f62ce8039357794b32e2fd6028e8268d89
chydeepak7 <deepakchy793@gmail.com>
2024-06-01 18:05:51 +0545

    just learning

--- classbased/practice/views.py
+++ classbased/practice/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+from .models import *
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+
+class Myview(View):
+    def get(self,request):
+        return render(request,'html/test.html')
+
+class GreekCreate(CreateView):
+    model = GeeksModel
+    fields = '__all__'
+
+
+
